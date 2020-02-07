1553ced39d14e8c2695da565379f53132059e7d9
Tham <zarub2k@gmail.com>
2020-02-07 07:22:30 +0530

    Config work started

--- src/main/java/com/hustlebar/helidonbar/ping/IPingApi.java
+++ src/main/java/com/hustlebar/helidonbar/ping/IPingApi.java
@@ -2,9 +2,16 @@ package com.hustlebar.helidonbar.ping;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 @Path("/ping")
 public interface IPingApi {
     @GET
     String ping();
+
+    @GET
+    @Path("welcome")
+    String welcome(@QueryParam("name") String name);
 }
--- src/main/java/com/hustlebar/helidonbar/ping/PingApi.java
+++ src/main/java/com/hustlebar/helidonbar/ping/PingApi.java
@@ -1,11 +1,27 @@
 package com.hustlebar.helidonbar.ping;
 
+import org.eclipse.microprofile.config.Config;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import java.text.MessageFormat;
 
 @RequestScoped
 public class PingApi implements IPingApi {
+    @Inject Config config;
+
     @Override
     public String ping() {
         return "Welcome to Helidon bar!";
+    }
+
+    @Override
+    public String welcome(String name) {
+        String welcomeValue = config.getValue("app.welcome", String.class);
+        System.out.println(welcomeValue);
+
+        String formattedValue = MessageFormat.format(welcomeValue, name);
+        System.out.println(formattedValue);
+        return formattedValue;
     }
 }
