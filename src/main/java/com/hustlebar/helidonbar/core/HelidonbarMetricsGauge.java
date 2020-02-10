fecb3963a6d098452422bb77b7bda9a5ba6308d6
Tham <zarub2k@gmail.com>
2020-02-10 19:19:34 +0530

    Gauge is enabled

---
+++ src/main/java/com/hustlebar/helidonbar/core/HelidonbarMetricsGauge.java
@@ -0,0 +1,24 @@
+package com.hustlebar.helidonbar.core;
+
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicLong;
+
+@ApplicationScoped
+public class HelidonbarMetricsGauge {
+    private AtomicLong startTime = new AtomicLong(0);
+
+    public void onStartUp(@Observes @Initialized(ApplicationScoped.class) Object init) {
+        startTime = new AtomicLong(System.currentTimeMillis());
+    }
+
+    @Gauge(unit = MetricUnits.SECONDS)
+    public long upTime() {
+        return Duration.ofMillis(System.currentTimeMillis() - startTime.get()).getSeconds();
+    }
+}
